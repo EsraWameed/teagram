306719463a3ec86e8626dc1a2def83ef588b846d
Esra Al <esra.wameed@yahoo.com>
2022-10-28 14:10:05 -0400

    views render

--- controllers/homepage-route.js
+++ controllers/homepage-route.js
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
 
         // Serialize data so the template can read it
         const posts = PostData.map((project) => project.get({ plain: true }));
-
+        console.log(posts);
         // Pass serialized data and session flag into template
         res.render('homepage', {
             posts,

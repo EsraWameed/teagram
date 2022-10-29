82ef1a4555b1d923a7897e2d701eebe787cb42b6
Abed Hasan <abedhasan79@gmail.com>
2022-10-29 19:45:20 -0400

    NO BUGSSSSSSSSSSS

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+router.get('/', async (req, res) => {
+    try {
+        const userData = await User.findAll();
+        res.status(200).json(userData);
 
-
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 // CREATE new user
 router.post('/', async (req, res) => {

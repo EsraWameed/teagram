ced32b126ef14149aacc6689fab0c3e70dd326ba
Abed Hasan <abedhasan79@gmail.com>
2022-10-30 16:21:04 -0400

    added user authentication to all post put and delete routes

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.set('view engine', 'handlebars');
 app.use(express.static("public"));
 app.use(express.static('controllers/resource'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,6 @@
 const withAuth = (req, res, next) => {
 
-  if (!req.session.logged_in) {
+  if (!req.session.loggedIn) {
     res.redirect('/login');
   } else {
     next();
